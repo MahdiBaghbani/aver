c4abdb95ae7536aff0ceb6e62f90d8da80046cde
Mahdi Baghbani <mahdi-baghbani@azadehafzar.io>
2024-08-31 12:09:37 +0330

    add: database models

--- crates/server/src/database/mod.rs
+++ crates/server/src/database/mod.rs
@@ -1,12 +1,27 @@
-use native_db::Models;
-use once_cell::sync::Lazy;
+use crate::settings::settings;
+use native_db::{Builder, Database, Models};
+use once_cell::sync::{Lazy, OnceCell};
 
 pub mod models;
 
-static DATABASE_MODELS: Lazy<Models> = Lazy::new(|| {
-    let mut models = Models::new();
+pub static DATABASE_MODELS: Lazy<Models> = Lazy::new(|| {
+    let mut models: Models = Models::new();
     models
-        .define::<models::User>()
+        .define::<models::user::v1::User>()
         .expect("failed to define model User");
     models
 });
+
+pub static DATABASE: OnceCell<Database> = OnceCell::new();
+
+pub fn database() -> &'static Database<'static> {
+    DATABASE.get().expect("database init")
+}
+
+pub fn init() {
+    let database: Database = Builder::new().create(
+        &DATABASE_MODELS,
+        settings().database.redb.path.clone(),
+    ).unwrap();
+    DATABASE.set(database).ok();
+}
---
+++ crates/server/src/database/models/user.rs
@@ -0,0 +1,22 @@
+use native_db::*;
+use native_model::{native_model, Model};
+use serde::{Deserialize, Serialize};
+
+pub mod v1 {
+    use super::*;
+    #[derive(Serialize, Deserialize, PartialEq, Debug)]
+    #[native_model(id = 1, version = 1)]
+    #[native_db]
+    pub struct User {
+        #[primary_key]
+        pub id: String,
+        #[secondary_key]
+        pub username: String,
+        pub password: String,
+        pub first_name: String,
+        pub last_name: String,
+        pub email: Option<String>,
+        pub created_at: String,
+        pub updated_at: String,
+    }
+}
