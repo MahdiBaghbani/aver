15c6b1bf200c3a59df04003b6e04a84701187558
Mahdi Baghbani <mahdi-baghbani@azadehafzar.io>
2024-09-01 22:55:23 +0330

    add: users service and state to application

--- crates/server/src/http/mod.rs
+++ crates/server/src/http/mod.rs
@@ -11,14 +11,17 @@ pub mod middlewares;
 use crate::http::middlewares::auth::endpoints::create_token;
 use crate::http::services::ocm::endpoints::legacy_discovery;
 use crate::http::services::ocm::router::ocm;
+use crate::http::services::users::router::users;
 use crate::http::services::wellknown::router::wellknown;
+use crate::models::ApplicationState;
 use crate::settings::settings;
 
-pub fn application() -> impl Endpoint {
+pub fn application(state: ApplicationState) -> impl Endpoint {
     Route::new()
         .nest("/", services())
         .with(NormalizePath::new(TrailingSlash::Trim))
         .with(CatchPanic::new().with_handler(PanicHandler::new()))
+        .data(state)
         .around(log)
         .boxed()
 }
@@ -27,7 +30,8 @@ fn services() -> impl Endpoint {
     Route::new()
         .nest("/", root())
         .nest("/.well-known", wellknown())
-        .nest(settings().ocm.provider.prefix.clone(), ocm())
+        .nest(settings().ocm.provider.get_prefix(), ocm())
+        .nest("/users", users())
 }
 
 fn root() -> impl Endpoint {
