3456f5d996bc21f2c64f370effd94e089dd20ebc
Mahdi Baghbani <mahdi-baghbani@azadehafzar.io>
2024-09-03 23:36:38 +0330

    style: imports

--- crates/server/src/settings/methods.rs
+++ crates/server/src/settings/methods.rs
@@ -5,7 +5,19 @@ use crate::http::services::ocm::models::{
     DiscoveryProtocols,
     DiscoveryResourceTypes,
 };
-use crate::settings::models::{Database, Ocm, OcmProvider, OcmProviderCapabilities, OcmProviderProtocols, OcmProviderResourceTypes, OcmProviderShareTypes, Server, Session, Settings};
+
+use super::models::{
+    Database,
+    Ocm,
+    OcmProvider,
+    OcmProviderCapabilities,
+    OcmProviderProtocols,
+    OcmProviderResourceTypes,
+    OcmProviderShareTypes,
+    Server,
+    Session,
+    Settings,
+};
 
 impl Settings {
     pub fn new() -> Result<Self, ConfigError> {
